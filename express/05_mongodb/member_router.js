391fa143c12b85cca50e918fbd24e6774d27e60f
minjun91a <vchocom666@gmail.com>
2026-08-28 14:10:56 +0900

    회원 조회

--- express/05_mongodb/member_router.js
+++ express/05_mongodb/member_router.js
@@ -1,33 +1,66 @@
 const express = require('express');
 const router = express.Router();
+const Member = require('./model');
 
 // 회원가입 (/member/join)
-router.post('/join', (req, res) => {
-    const param = req.body;
-    res.json({'success':true, 'data':param});
+router.post('/join',async (req, res) => {
+
+    const {id, pw, name, phone} = req.body;
+    try{
+        let result = await Member.create({id, pw, name, phone});
+        let object = result.toObject();
+        delete object.pw;   // pw 는 결과값에서 제거하고 보여준다.
+        // object.pw = '';
+        res.json({'success': true, 'data': object});
+    } catch (e) {
+        console.error(e, 'CODE :'+e.code);
+
+        let msg = '';
+
+        switch (e.code){
+            case 11000:
+                msg = "이미 사용중인 아이디 입니다.";
+                break;
+
+            default:
+                msg = "필수값을 확인해 주세요";
+        }
+
+        res.json({'success':false,message:msg});
+    }
 });
 
 // 회원 리스트 (/member/list, /member/)
-router.get(['/list', '/'], (req, res) => {
-    res.json({'success':true, 'data':[]});
+router.get(['/list', '/'],async (req, res) => {
+    let list = await Member.find()
+        .sort({'createdAt':-1})     // 생성일 내림차순으로 정렬
+        .lean();                         // 순수 JSON 으로 반환
+
+    res.json({'success': true, 'data': list});
 });
 
 // 회원정보 상세보기 (/member/get/:id)
-router.get('/get/:id', (req, res) => {
+router.get('/get/:id',async (req, res) => {
     const {id} = req.params;
-    res.json({'success':true, 'data':{'id':id, 'msg':'상세보기 완료'}});
+    // 찾는 내용이 하나일 경우는 findOne({filter}) 사용
+    let member = await Member.findOne({id}).lean();
+
+    if (member == null) {
+        res.json({'success': false, 'data':{'info':{}, 'msg':'없는 회원'}});
+    };
+    res.json({'success': true, 'data': {'info': member, 'msg': '상세보기 완료'}});
 });
 
 // 회원정보 수정 (/member/update/:id)
 router.put('/update/:id', (req, res) => {
     const {id} = req.params;
     const param = req.body;
-    res.json({'success':true, 'data':{'id':id, 'msg':param}});
+    res.json({'success': true, 'data': {'id': id, 'msg': param}});
 });
 
 // 회원 삭제 (/member/delete/:id)
 router.delete('/delete/:id', (req, res) => {
-    res.json({'success':true, 'data':'회원삭제 완료'});
+    res.json({'success': true, 'data': '회원삭제 완료'});
 });
 
 module.exports = router;
