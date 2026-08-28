a06695fab1d2a3b971a27a570656e0bc8d5acbc5
minjun91a <vchocom666@gmail.com>
2026-08-28 10:15:12 +0900

    DB 접속

--- express/05_mongodb/app.js
+++ express/05_mongodb/app.js
@@ -2,12 +2,15 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const connectDB = require('./db');
 
 // middle ware
 // Cross Domain Policy(JS 를 이용한 서로 다른 도메인에서 하는 통신을 막는다.)
 // 그래서 특정 IP 에 대해서 허용해 주는 기능
 app.use(cors());
 app.use(express.json()) // body로 보내는 JSON 형태로 받기 위해 사용
+
+connectDB();    // DB 접속 먼저
 
 app.all('/', (req, res) => {
     res.send('/member 를 이용해 join, list, get, update, delete');
