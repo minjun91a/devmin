e49acc9ac16bb51687c35f9f9a10d585c07a9241
minjun91a <vchocom666@gmail.com>
2026-08-28 11:04:25 +0900

    http body 전송테스트

--- express/05_mongodb/member_router.js
+++ express/05_mongodb/member_router.js
@@ -3,7 +3,8 @@ const router = express.Router();
 
 // 회원가입 (/member/join)
 router.post('/join', (req, res) => {
-    res.json({'success':true, 'data':'회원가입 완료'});
+    const param = req.body;
+    res.json({'success':true, 'data':param});
 });
 
 // 회원 리스트 (/member/list, /member/)
@@ -20,7 +21,8 @@ router.get('/get/:id', (req, res) => {
 // 회원정보 수정 (/member/update/:id)
 router.put('/update/:id', (req, res) => {
     const {id} = req.params;
-    res.json({'success':true, 'data':{'id':id, 'msg':'수정 완료'}});
+    const param = req.body;
+    res.json({'success':true, 'data':{'id':id, 'msg':param}});
 });
 
 // 회원 삭제 (/member/delete/:id)
